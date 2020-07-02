008807a9e002c183571a4e895ae4f227e4bc0ae3
Yamamoto Taijiro <taijiro@tama.net>
2020-07-02 10:00:46 +0900

    Chatアプリ

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const PORT = 7000;
+
+app.use(express.static('public'));
+
+io.on('connection',function(socket){
+    socket.on('message',function(msg){
+        io.emit('message', msg);
+    });
+});
+
+http.listen(PORT, function(){
+    console.log('server listening. Port:' + PORT);
+});
