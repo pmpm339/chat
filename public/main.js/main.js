a708e98de87351dda7a30d8a24ab2d401baa2b96
Misaki Imai <218c0062@s.obirin.ac.jp>
2020-07-09 18:23:38 +0900

    チャットの送信者がわかるように、データの構造をただの文字からJSONに変更

--- public/main.js/main.js
+++ public/main.js/main.js
@@ -1,27 +1,16 @@
-    const socketio = io();
-    const form = document.getElementById("form");
-    const input = document.getElementById("msg");
-    const chats = document.getElementById("chats");
+const socketio = io();
+const form = document.getElementById("form");
+const input = document.getElementById("msg");
+const chats = document.getElementById("chats");
 
-    const nameform = document.getElementById("nameform");
-const name = document.getElementById("name");
-
-let username='';
-nameform.addEventListener('submit', function(event){
-  username = name.value;
-  event.preventDefault();
-  nameform.style.display ="none";
-  form.style.display ="block";
-})
-
-    form.addEventListener('submit', function(event){
-        const msg = JSON.stringify({msg: input.value, name: username})
+form.addEventListener('submit', function(event){
+  const msg = JSON.stringify({msg: input.value, name: 'user'})
   socketio.emit('message', msg);
-      input.value='';
-      event.preventDefault();
-    })
-    socketio.on('message',function(msg){
-        const obj = JSON.parse(msg);
+  input.value='';
+  event.preventDefault();
+})
+socketio.on('message',function(msg){
+  const obj = JSON.parse(msg);
 
   const dt = document.createElement("dt");
   const dd = document.createElement("dd");
@@ -29,4 +18,4 @@ nameform.addEventListener('submit', function(event){
   chats.append(dt);
   dd.append(obj.msg);
   chats.append(dd);
-    });
+});
