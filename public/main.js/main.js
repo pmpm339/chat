1509e78dfd82f37e17a3bd316b1bfc7825c35a54
Misaki Imai <218c0062@s.obirin.ac.jp>
2020-07-09 18:17:10 +0900

    javascriptを別ファイルにした

--- public/main.js/main.js
+++ public/main.js/main.js
@@ -1,34 +1,32 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width,initial-scale=1">
-    <title>Obirin2020 chat</title>
-    <script src="/socket.io/socket.io.js"></script>
-    <link href="style.css" rel="stylesheet" type="text/css">
-</head>
-<body>
-    <h1>チャット</h1>
-    <ul id="chats"></ul>
-    <form id="form">
-      <input id="msg" autocomplete="off" /><button>送信</button>
-    </form>
-  <script>
     const socketio = io();
     const form = document.getElementById("form");
     const input = document.getElementById("msg");
     const chats = document.getElementById("chats");
 
+    const nameform = document.getElementById("nameform");
+const name = document.getElementById("name");
+
+let username='';
+nameform.addEventListener('submit', function(event){
+  username = name.value;
+  event.preventDefault();
+  nameform.style.display ="none";
+  form.style.display ="block";
+})
+
     form.addEventListener('submit', function(event){
-      socketio.emit('message', input.value);
+        const msg = JSON.stringify({msg: input.value, name: username})
+  socketio.emit('message', msg);
       input.value='';
       event.preventDefault();
     })
     socketio.on('message',function(msg){
-      const li = document.createElement("li");
-      li.append(msg);
-      chats.append(li);
+        const obj = JSON.parse(msg);
+
+  const dt = document.createElement("dt");
+  const dd = document.createElement("dd");
+  dt.append(obj.name);
+  chats.append(dt);
+  dd.append(obj.msg);
+  chats.append(dd);
     });
-  </script>
-</body>
-</html>
