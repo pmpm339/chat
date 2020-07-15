ff75ea0fab7b6106e898618c5fa1d7a42e832e78
Misaki Imai <218c0062@s.obirin.ac.jp>
2020-07-15 16:18:44 +0900

    表示名を入れて参加してからチャット始めるようにした

--- public/main.js/main.js
+++ public/main.js/main.js
@@ -3,8 +3,19 @@ const form = document.getElementById("form");
 const input = document.getElementById("msg");
 const chats = document.getElementById("chats");
 
+const nameform = document.getElementById("nameform");
+const name = document.getElementById("name");
+
+let username='';
+nameform.addEventListener('submit', function(event){
+  username = name.value;
+  event.preventDefault();
+  nameform.style.display ="none";
+  form.style.display ="block";
+})
+
 form.addEventListener('submit', function(event){
-  const msg = JSON.stringify({msg: input.value, name: 'user'})
+  const msg = JSON.stringify({msg: input.value, name: username})
   socketio.emit('message', msg);
   input.value='';
   event.preventDefault();
