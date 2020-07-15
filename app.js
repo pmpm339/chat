7ddf92f3e6e7c5c95c41c9d811783416a2eba63e
Misaki Imai <218c0062@s.obirin.ac.jp>
2020-07-15 16:26:43 +0900

    メッセージをサーバ側で記憶するようにして、サインイン時に過去のメッセージを受け取るようにした

--- app.js
+++ app.js
@@ -6,8 +6,18 @@ const PORT = 3000;
 
 app.use(express.static('public'));
 
+let messages = []; // 過去のメッセージを保存しておく配列
+
 io.on('connection',function(socket){
+    socket.on('signin',function(){
+        // 参加時に過去のメッセージを返す
+        const id = socket.id;
+        io.to(id).emit('signin', messages);
+    });
+
     socket.on('message',function(msg){
+        messages.push(msg); // メッセージを配列に追加
+        messages=messages.slice(-100); // 最新の100件だけ保存
         io.emit('message', msg);
     });
 });
