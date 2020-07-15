54e93a86660074bd35ee976c71de1e40f2756abf
Misaki Imai <218c0062@s.obirin.ac.jp>
2020-07-15 16:16:59 +0900

    チャットの送信者がわかるように、データの構造をただの文字からJSONに変更

--- public/main.js/main.js
+++ public/main.js/main.js
@@ -4,12 +4,18 @@ const input = document.getElementById("msg");
 const chats = document.getElementById("chats");
 
 form.addEventListener('submit', function(event){
-  socketio.emit('message', input.value);
+  const msg = JSON.stringify({msg: input.value, name: 'user'})
+  socketio.emit('message', msg);
   input.value='';
   event.preventDefault();
 })
 socketio.on('message',function(msg){
-  const li = document.createElement("li");
-  li.append(msg);
-  chats.append(li);
+  const obj = JSON.parse(msg);
+
+  const dt = document.createElement("dt");
+  const dd = document.createElement("dd");
+  dt.append(obj.name);
+  chats.append(dt);
+  dd.append(obj.msg);
+  chats.append(dd);
 });
