ac886249e0776424b73a862ab3132d91506c57e2
Misaki Imai <218c0062@s.obirin.ac.jp>
2020-07-15 16:15:55 +0900

    javascriptを別ファイルにした

--- public/main.js/main.js
+++ public/main.js/main.js
@@ -3,30 +3,13 @@ const form = document.getElementById("form");
 const input = document.getElementById("msg");
 const chats = document.getElementById("chats");
 
-const nameform = document.getElementById("nameform");
-const name = document.getElementById("name");
-
-let username='';
-nameform.addEventListener('submit', function(event){
-  username = name.value;
-  event.preventDefault();
-  nameform.style.display ="none";
-  form.style.display ="block";
-})
-
 form.addEventListener('submit', function(event){
-  const msg = JSON.stringify({msg: input.value, name: username})
-  socketio.emit('message', msg);
+  socketio.emit('message', input.value);
   input.value='';
   event.preventDefault();
 })
 socketio.on('message',function(msg){
-  const obj = JSON.parse(msg);
-
-  const dt = document.createElement("dt");
-  const dd = document.createElement("dd");
-  dt.append(obj.name);
-  chats.append(dt);
-  dd.append(obj.msg);
-  chats.append(dd);
+  const li = document.createElement("li");
+  li.append(msg);
+  chats.append(li);
 });
